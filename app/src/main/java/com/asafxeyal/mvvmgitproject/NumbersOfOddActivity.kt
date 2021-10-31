099a7f8daf9d5bd6c56fcd11bd3d1365da120189
Asaf <asefhacioglu17@gmail.com>
2021-10-31 17:00:00 +0400

    Added NumbersOfOddActivity.kt

---
+++ app/src/main/java/com/asafxeyal/mvvmgitproject/NumbersOfOddActivity.kt
@@ -0,0 +1,11 @@
+package com.asafxeyal.mvvmgitproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NumbersOfOddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numbers_of_odd)
+    }
+}
